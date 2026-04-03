4a9d64aff4e336f6f54eb4258612c8c966a19229
Johann Visagie <johann@visagie.za.net>
2026-04-03 20:55:19 +0200

    Update lx to 0.6.3

--- Formula/lx.rb
+++ Formula/lx.rb
@@ -1,43 +1,31 @@
 class Lx < Formula
   desc "A modern file lister with personality"
   homepage "https://github.com/wjv/lx"
-  version "0.6.1"
+  version "0.6.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wjv/lx/releases/download/v0.6.1/lx-aarch64-apple-darwin"
-      sha256 "79da13da61e3b0ea283760d751dca5f545305781c9d0cb5dbf31652ddf1e8781"
+      url "https://github.com/wjv/lx/releases/download/v0.6.3/lx-aarch64-apple-darwin"
+      sha256 "9335d45b53e7de90a61225e29c8e739cbe342f18d136e5b7107841e30992c1f0"
     else
-      url "https://github.com/wjv/lx/releases/download/v0.6.1/lx-x86_64-apple-darwin"
-      sha256 "96990c532bdb0df39fdc0a4018db37bf4173a7532b230fd02aa34e4edd3b91fd"
+      url "https://github.com/wjv/lx/releases/download/v0.6.3/lx-x86_64-apple-darwin"
+      sha256 "53d67ede06280d3c367bdc3f576b5a7b7992fc2f039fd461a1a0e8a40fbe73d6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/wjv/lx/releases/download/v0.6.1/lx-aarch64-unknown-linux-gnu"
-      sha256 "46324d59cb55f0bbe2351e146063068a0e038bd3b59515830b8a14e5bf26acf0"
+      url "https://github.com/wjv/lx/releases/download/v0.6.3/lx-aarch64-unknown-linux-gnu"
+      sha256 "ac3d04fe908a8ce738b671ab9195a140b5e47656dee5b9470b1c3b1e1c2bc936"
     else
-      url "https://github.com/wjv/lx/releases/download/v0.6.1/lx-x86_64-unknown-linux-gnu"
-      sha256 "0bd762ede42b249143f80864ca98375e5dc7dc968b8e8f2399062e9abddd4eed"
+      url "https://github.com/wjv/lx/releases/download/v0.6.3/lx-x86_64-unknown-linux-gnu"
+      sha256 "3e18f81f2a45b4ea3631bd9d174edbddc3ed5bbf6f287d2bd9a48a3886854cc8"
     end
-  end
-
-  resource "man-lx" do
-    url "https://github.com/wjv/lx/releases/download/v0.6.1/lx.1"
-    sha256 ""  # TODO: fill once man pages are in release assets
-  end
-
-  resource "man-lxconfig" do
-    url "https://github.com/wjv/lx/releases/download/v0.6.1/lxconfig.toml.5"
-    sha256 ""  # TODO: fill once man pages are in release assets
   end
 
   def install
     bin.install Dir["lx-*"].first => "lx"
-    man1.install resource("man-lx") => "lx.1"
-    man5.install resource("man-lxconfig") => "lxconfig.toml.5"
   end
 
   test do
